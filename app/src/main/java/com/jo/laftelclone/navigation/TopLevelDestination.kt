93065b93269d776104beffb83aa0c75dedbba101
akak4456 <akak4456@naver.com>
2025-01-02 18:45:05 +0900

    [ADD] main bottom nav icon 및 text 구현함

--- app/src/main/java/com/jo/laftelclone/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/jo/laftelclone/navigation/TopLevelDestination.kt
@@ -1,29 +1,43 @@
 package com.jo.laftelclone.navigation
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.jo.feature.box.navigation.BoxRoute
 import com.jo.feature.home.navigation.HomeRoute
 import com.jo.feature.my.navigation.MyRoute
 import com.jo.feature.tagsearch.navigation.TagSearchRoute
+import com.jo.laftelclone.R
 
 enum class TopLevelDestination(
+    @DrawableRes val selectedIcon: Int,
+    @DrawableRes val unselectedIcon: Int,
     @StringRes val iconTextId: Int,
-    val route: String
+    val route: String,
+    val isMyIcon: Boolean = false
 ) {
     HOME(
+        selectedIcon = R.drawable.ic_home_on,
+        unselectedIcon = R.drawable.ic_home_off,
         iconTextId = com.jo.feature.home.R.string.home_title,
         HomeRoute
     ),
     BOX(
+        selectedIcon = R.drawable.ic_box_on,
+        unselectedIcon = R.drawable.ic_box_off,
         iconTextId = com.jo.feature.box.R.string.box_title,
         BoxRoute
     ),
     TAG_SEARCH(
+        selectedIcon = R.drawable.ic_tagsearch_on,
+        unselectedIcon = R.drawable.ic_tagsearch_off,
       iconTextId = com.jo.feature.tagsearch.R.string.tagsearch_title,
         TagSearchRoute
     ),
     MY(
+        selectedIcon = 0,
+        unselectedIcon = 0,
         iconTextId = com.jo.feature.my.R.string.my_title,
-        MyRoute
+        MyRoute,
+        isMyIcon = true
     )
 }
--- app/src/main/java/com/jo/laftelclone/ui/LaftelCloneApp.kt
+++ app/src/main/java/com/jo/laftelclone/ui/LaftelCloneApp.kt
@@ -1,5 +1,9 @@
 package com.jo.laftelclone.ui
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -7,17 +11,29 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.jo.feature.my.navigation.navigateToMy
+import com.jo.laftelclone.R
 import com.jo.laftelclone.navigation.LaftelCloneNavHost
 import com.jo.laftelclone.navigation.TopLevelDestination
+import com.jo.laftelclone.ui.theme.LaftelBaseColor
+import com.jo.laftelclone.ui.theme.LaftelBlackColor
 
 @Composable
 internal fun LaftelCloneApp(
@@ -32,12 +48,19 @@ internal fun LaftelCloneApp(
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
-    NavigationBar {
+    NavigationBar(containerColor = Color.White, contentColor = Color.White) {
         val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
         TopLevelDestination.entries.forEach { item ->
+            val selected = currentRoute == item.route
             NavigationBarItem(
-                selected = currentRoute == item.route,
-                icon = { Icons.Default.Home },
+                selected = selected,
+                icon = { Image(
+                    painter = painterResource(
+                        if(item.isMyIcon) R.drawable.ic_my_example // TODO my icon glide 등으로 호출하기
+                        else if(selected) item.selectedIcon else item.unselectedIcon
+                    ),
+                    contentDescription = null
+                )},
                 label = { Text(stringResource(item.iconTextId)) },
                 onClick = {
                     if(currentRoute != item.route) {
@@ -49,7 +72,16 @@ fun BottomNavigationBar(navController: NavHostController) {
                             restoreState = true
                         }
                     }
-                }
+                },
+                colors = NavigationBarItemColors(
+                    selectedIconColor = Color.White,
+                    selectedTextColor = LaftelBaseColor,
+                    selectedIndicatorColor = Color.White,
+                    unselectedIconColor = Color.White,
+                    unselectedTextColor = LaftelBlackColor,
+                    disabledIconColor = Color.White,
+                    disabledTextColor = LaftelBlackColor,
+                )
             )
         }
     }
