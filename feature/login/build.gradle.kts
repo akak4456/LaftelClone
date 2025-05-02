8baed15c6012368b67f5e4f11bec3a18335dd183
akak4456 <akak4456@naver.com>
2025-05-02 19:59:04 +0900

    [CNG] LoginScreen UI 구성함 & Screenshot test 추가함

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.jo.feature)
+    alias(libs.plugins.jo.hilt)
 }
 
 android {
--- feature/login/src/main/java/com/jo/laftelclone/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/jo/laftelclone/feature/login/LoginScreen.kt
@@ -1,19 +1,118 @@
 package com.jo.laftelclone.feature.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.jo.laftelclone.core.designsystem.theme.Primary
 import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 internal fun LoginScreen() {
+    LoginScreen(true)
+}
+
+@Composable
+internal fun LoginScreen(hasGifBackground: Boolean) {
     Box(modifier = Modifier.fillMaxSize()) {
-        GlideImage(
-            imageModel = R.drawable.bg_login,
-            contentScale = ContentScale.Crop,
-        )
+        if(hasGifBackground) {
+            GlideImage(
+                imageModel = R.drawable.bg_login,
+                contentScale = ContentScale.Crop,
+            )
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize().background(color = Color.Black),
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.55f),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        stringResource(R.string.feature_login_big_title),
+                        color = Color.White,
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            lineHeight = 40.sp
+                        ),
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.fillMaxHeight(0.05f))
+                    Text(
+                        stringResource(R.string.feature_login_small_title),
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                    Spacer(modifier = Modifier.fillMaxHeight(0.05f))
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp),
+                        shape = RoundedCornerShape(8.dp), // radius 설정
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Primary
+                        )
+                    ) {
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = stringResource(R.string.feature_login_start_email))
+
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_email),
+                                contentDescription = "Email Icon",
+                                modifier = Modifier
+                                    .align(Alignment.CenterStart)
+                                    .width(30.dp)
+                                    .height(24.dp),
+                                tint = Color.Unspecified
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
